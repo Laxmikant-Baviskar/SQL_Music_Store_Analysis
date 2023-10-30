2a23a9536082ca0aa1736a2a4603bbc476927b4a
Laxmikant-Baviskar <99134289+Laxmikant-Baviskar@users.noreply.github.com>
2023-10-30 23:51:09 +0530

    Update Music_Store_Query.sql

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -25,7 +25,7 @@ ORDER BY total DESC
 
 /* Q4: Which city has the best customers? We would like to throw a promotional Music Festival in the city we made the most money. 
 Write a query that returns one city that has the highest sum of invoice totals. 
-Return both the city name & sum of all invoice totals */
+Return both the city name & sum of all invoice totals */ 
 
 SELECT billing_city,SUM(total) AS InvoiceTotal
 FROM invoice
